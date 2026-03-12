6f95b5c218c3e01b886744f434c2f76d181343a1
Navein Niranjan <navein.niranjan@ontariotechu.net>
2026-03-11 21:29:26 -0400

    Minor changes to file size maximum

--- Frontend/src/components/BookFormModal.jsx
+++ Frontend/src/components/BookFormModal.jsx
@@ -23,8 +23,8 @@ export function BookFormModal({ open, onClose, onSave }) {
   const handleFileChange = (e) => {
     const file = e.target.files[0]
     if (!file) return
-    if (file.size > 2 * 1024 * 1024) {
-    setError("Image must be under 2MB.");
+    if (file.size > 20 * 1024 * 1024) {
+    setError("Image must be under 20MB.");
     return;
   }
   setError("")
